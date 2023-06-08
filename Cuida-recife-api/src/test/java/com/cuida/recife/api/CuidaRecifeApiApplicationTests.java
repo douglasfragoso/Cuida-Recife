1f0b9d4fe71dd09adf560eb6b82e36f025eb699b
Douglas Fragoso <douglas.iff@gmail.com>
2023-06-07 21:27:36 -0300

    Criação do projeto em Spring Boot

---
+++ Cuida-recife-api/src/test/java/com/cuida/recife/api/CuidaRecifeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuida.recife.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuidaRecifeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
